c5d78267092ce2d8687338b073bf0f8a586495b5
Hleb Chupin <chupin.gleb@me.com>
2019-05-27 12:39:51 +0300

    third changes

--- lessoneight/UniqueWordsFinder.java
+++ lessoneight/UniqueWordsFinder.java
@@ -1,34 +1,36 @@
 package lessoneight;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class UniqueWordsFinder {
 
-    public int FindUniqueWords(String path) {
-        HashSet filter = null;
-        try {
-            BufferedReader inFiles = new BufferedReader(new FileReader(path));
-            ArrayList<String> sentences = new ArrayList<String>();
+    private static HashSet uniqueWordsCount = new HashSet();
+    ;
+
+    public void UniqueWordsCounter(String path, String nameFilter) {
+        File folder = new File(path);
+        String[] buferTwo = folder.list(new FolderFileNameFilter(nameFilter));
+        for (String s : buferTwo) {
+            UniqueWordsFinder(folder + "/" + s);
+        }
+        System.out.println(uniqueWordsCount.size());
+    }
+
+    private void UniqueWordsFinder(String path) {
+        try (BufferedReader inFiles = new BufferedReader(new FileReader(path))) {
             while (inFiles.ready()) {
-                sentences.add(inFiles.readLine());
-            }
-            inFiles.close();
-            filter = new HashSet();
-            for (int i = 0; i < sentences.size(); i++) {
-                StringTokenizer words = new StringTokenizer(sentences.get(i), ",. ");
+                StringTokenizer words = new StringTokenizer(inFiles.readLine(), ",. ");
                 while (words.hasMoreTokens()) {
-                    filter.add(words.nextToken());
+                    uniqueWordsCount.add(words.nextToken());
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            return filter.size();
         }
     }
 }
--- lessoneight/main.java
+++ lessoneight/main.java
@@ -9,39 +9,28 @@ public class Main {
         /*
         /Task 1
          */
-        InputStream in = null;
-        OutputStream out = null;
-        try {
-            in = new FileInputStream("/Users/hlebchupin/Downloads/IMG_3321.mov");
-            byte[] buffer = new byte[in.available()];
+        byte[] buffer = null;
+        try (InputStream in = new FileInputStream("/Users/hlebchupin/Downloads/IMG_3321.mov")) {
+            buffer = new byte[in.available()];
             in.read(buffer,
                     0,
                     buffer.length);
-            out = new FileOutputStream("/Users/hlebchupin/Downloads/IMG_3321_copy.mov");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (OutputStream out = new FileOutputStream("/Users/hlebchupin/Downloads/IMG_3321_copy.mov")) {
             out.write(buffer,
                     0,
                     buffer.length);
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+
 
         /*
         /Task 2
          */
-        BufferedReader text = null;
-        try {
-            text = new BufferedReader(new FileReader("/Users/hlebchupin/Downloads/Text.txt"));
+        try (BufferedReader text = new BufferedReader(new FileReader("/Users/hlebchupin/Downloads/Text.txt"))) {
             TreeSet<String> sort = new TreeSet<String>();
             while (text.ready()) {
                 sort.add(text.readLine().toString());
@@ -51,53 +40,41 @@ public class Main {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                text.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+
 
         /*
         /Task 3
          */
 
         File folder = null;
-        BufferedReader read = null;
-        try {
-            folder = new File("/Users/hlebchupin/Documents/Java/chupin/booklearning/src/lessonseven");
-            String[] bufer = folder.list(new FolderFileNameFilter(".java"));
-            int o = 1;
-            for (String s : bufer) {
-                System.out.println("File number " + o + " :");
-                System.out.println("File name is: " + s);
-                read = new BufferedReader(new FileReader(folder + "/" + s));
+        folder = new
+
+                File("/Users/hlebchupin/Documents/Java/chupin/booklearning/src/lessonseven");
+
+        String[] bufer = folder.list(new FolderFileNameFilter(".java"));
+        int o = 1;
+        for (
+                String s : bufer) {
+            System.out.println("File number " + o + " :");
+            System.out.println("File name is: " + s);
+            try (BufferedReader read = new BufferedReader(new FileReader(folder + "/" + s))) {
                 for (int i = 0; i < 3; i++) {
                     System.out.println(read.readLine());
                 }
                 o++;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                read.close();
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-         /*
-        /Task 4
-         */
-        folder = new File("/Users/hlebchupin/Documents/Java/chupin/booklearning/src/lessonseven");
-        String[] buferTwo = folder.list(new FolderFileNameFilter(".java"));
+
+    /*
+   /Task 4
+    */
         UniqueWordsFinder finder = new UniqueWordsFinder();
-        int overall = 0;
-        for (String s : buferTwo) {
-            overall += finder.FindUniqueWords(folder + "/" + s);
-        }
-        System.out.println(overall);
+        finder.UniqueWordsCounter("/Users/hlebchupin/Documents/Java/chupin/booklearning/src/lessonseven", ".java");
     }
 }
+
 
